afbfb3a5f60d61f4ae1dd1dc28b8c44366d489ce
Nikita <nikita-ahmetoff@yandex.ru>
2021-02-28 20:19:59 +0300

    Get dollar rate v. 0.1

---
+++ src/main/java/ExchangeRatesEntity.java
@@ -0,0 +1,27 @@
+import org.json.JSONObject;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class ExchangeRatesEntity {
+    private JSONObject rates;
+    String base;
+    Date date;
+    float value;
+
+    public ExchangeRatesEntity(String request) {
+        JSONObject tmp = new JSONObject(request.toString());
+
+        rates = tmp.getJSONObject("rates");
+        base = tmp.getString("base");
+        value = rates.getFloat("RUB");
+
+        java.util.Date utilDate = null;
+        try {
+            utilDate = new SimpleDateFormat("yyyy-MM-dd").parse(tmp.getString("date"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        date = new java.sql.Date(utilDate.getTime());
+    }
+}
